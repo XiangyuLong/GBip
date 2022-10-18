f788f1310922e3b22a2ac02b8ae663cd2a12c696
XiangyuLong <33380575+XiangyuLong@users.noreply.github.com>
2022-10-18 13:26:56 -0600

    Update
    Update

--- gbip_CohenD.m
+++
@@ -1,9 +0,0 @@
-function cohend = gbip_CohenD(data1,data2)
-
-% cohend = gbip_CohenD(data1,data2)
-
-dmean = mean(data1) - mean(data2);
-
-stdpooled = sqrt((std(data1)^2 + std(data2)^2)/2);
-
-cohend = abs(dmean/stdpooled);
--- gbip_rcutoff.m
+++
@@ -1,8 +0,0 @@
-function r_thr = gbip_rcutoff(p,dsize)
-
-% r_thr = gbip_rcutoff(p,dsize);
-% output is abs value of r threshold;
-% p should be 0.00X .
-
-rt = tinv(1-p/2,dsize-2);
-r_thr = rt/(sqrt(dsize-2+rt*rt));
